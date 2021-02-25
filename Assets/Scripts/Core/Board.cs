5cc600baa841de822ff90c6b63e5aa38db9fc2fe
RaymonRaymond Tan <ray.tangent42@gmail.com>
2021-02-25 09:43:03 -0500

    Uploading....

--- Assets/Scripts/Core/Board.cs
+++ Assets/Scripts/Core/Board.cs
@@ -53,7 +53,7 @@ public class Board : MonoBehaviour
             {
                 return false;
             }
-            // check whether pos is occupied or not.
+            // check whether pos is occupied or not. 
             if (IsOccupied((int)pos.x, (int)pos.y, shape))
             {
                 return false;
